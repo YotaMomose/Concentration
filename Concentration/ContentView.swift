0e71e052c1c8ed4bf9f6ad728ae06989bc7a0489
Yota Momose <tonkatsu440@gmail.com>
2023-11-26 10:01:13 +0900

    プレイ画面への遷移を実装
    navigationbartitleがいい感じじゃないため要修正

--- Concentration/ContentView.swift
+++ Concentration/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var path = NavigationPath()
     var body: some View {
-        VStack {
-            Text("神経衰弱")
-                .font(.largeTitle)
+        
+        NavigationStack(path: $path) {
+            VStack {
+                Text("神経衰弱")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                
+                NavigationLink("スタート", value: TransitionPath.play)
+            }
+            .navigationTitle(TransitionPath.start.rawValue)
+            .navigationDestination(for: TransitionPath.self, destination: { appended in
+                appended.Destination()
+                    .navigationTitle(appended.rawValue)
+                
+            })
+            .padding()
         }
-        .padding()
     }
 }
+
+///画面遷移の列挙型
+enum TransitionPath: String {
+    case start = "スタート"
+    case play  = "プレイ"
+
+    @ViewBuilder
+    func Destination() -> some View {
+        switch self {
+        case .start: ContentView()
+        case .play : PlayView()
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
---
+++ Concentration/PlayView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayView.swift
+//  Concentration
+//
+//  Created by 百瀬 陽太 on 2023/11/26.
+//
+
+import SwiftUI
+
+struct PlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayView()
+}
