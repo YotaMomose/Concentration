91f0a10973e059ed6c2a6207dcd83a8ccf5ee880
Yota Momose <tonkatsu440@gmail.com>
2023-11-30 21:28:16 +0900

    カードの画像とmodel、Viewを新規作成。 まだほぼ手をつけていない。

---
+++ Concentration/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Concentration
+//
+//  Created by 百瀬 陽太 on 2023/11/30.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ Concentration/CardViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CardViewModel.swift
+//  Concentration
+//
+//  Created by 百瀬 陽太 on 2023/11/30.
+//
+
+import Foundation
